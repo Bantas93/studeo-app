289221fab3ae82d37c29528e1464ac185f9cbb0e
Bantas93 <bantaz.mtdn@gmail.com>
2026-07-17 22:37:18 +0700

    delete useless file

--- server/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import AuthController from "../controllers/authController";
-
-const AuthRouter = express.Router();
-
-AuthRouter.post("/register", AuthController.register);
-
-export default AuthRouter;
