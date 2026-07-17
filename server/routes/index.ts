71fdd298c87faed6ef89d67aff2a116cb2289452
Bantas93 <bantaz.mtdn@gmail.com>
2026-07-17 22:36:09 +0700

    delete useless file

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import AuthRouter from "./auth";
+import express, { Request, Response } from "express";
 import UserRouter from "./users";
 
 const router = express.Router();
 
-router.use("/", AuthRouter);
+router.get("/", (req: Request, res: Response) => {
+  res.send("This is the studeo db!");
+});
+
 router.use("/users", UserRouter);
 
 export default router;
