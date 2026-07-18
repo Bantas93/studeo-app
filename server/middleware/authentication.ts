1e629c7be20ac793d3768e1ebb04b1d546f06436
Bantas93 <bantaz.mtdn@gmail.com>
2026-07-19 01:54:36 +0700

    create authentication

---
+++ server/middleware/authentication.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./errorHandler";
+import { verifyToken } from "../helpers/jwt";
+import User from "../models/User";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        name: string;
+        email: string;
+      };
+    }
+  }
+}
+
+export async function authentication(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new AppError("Invalid Token", 401);
+    }
+
+    const [tokenType, token] = authorization.split(" ");
+
+    if (tokenType !== "Bearer" || !token) {
+      throw new AppError("Invalid Token", 401);
+    }
+
+    const payload = verifyToken(token) as { _id: string; username: string };
+
+    const user = await User.find(payload._id);
+
+    if (!user) {
+      throw new AppError("Invalid Token", 401);
+    }
+
+    req.user = {
+      id: user._id,
+      name: user.username,
+      email: user.email,
+    };
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
