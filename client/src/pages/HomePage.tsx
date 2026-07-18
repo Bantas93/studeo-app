fed40612df088a0050005105405c2e9fc4d295b2
Bantas93 <bantaz.mtdn@gmail.com>
2026-07-19 05:38:01 +0700

    fetch data rooms on homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
+interface IRoom {
+  _id: string;
+  name: string;
+  subject: string;
+  roomType: string;
+  maxParticipants: number;
+}
+
 export default function HompePage() {
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/rooms`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        },
+      );
+
+      setRooms(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="min-h-screen p-6">
       <div className="flex flex-col gap-4">
-        <div>
-          <Link to={"/room/create"} className="btn">
+        <div className="flex gap-4">
+          <Link to={"/room/create"} className="btn btn-primary">
             Create Room
           </Link>
           <Link to={"/subject/create"} className="btn">
@@ -13,14 +47,29 @@ export default function HompePage() {
           </Link>
         </div>
 
-        <div className="card w-96 bg-base-100 card-md shadow-sm">
-          <div className="card-body">
-            <h2 className="card-title">Topic : Cari rumus segitiga</h2>
-            <p>Mata pelajaran : Matematika</p>
-            <div className="justify-end card-actions">
-              <button className="btn btn-primary">Join Room</button>
-            </div>
-          </div>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm-grid-cols-1 gap-4">
+          {rooms &&
+            rooms.map((room) => {
+              return (
+                <div
+                  className="card bg-base-100 card-md shadow-sm"
+                  key={room._id.toString()}
+                >
+                  <div className="card-body">
+                    <h2 className="card-title">Topic : {room.name}</h2>
+                    <p>Mata pelajaran : {room.subject}</p>
+                    <div className="justify-end card-actions">
+                      <Link
+                        to={`/room/${room._id.toString()}`}
+                        className="btn btn-primary"
+                      >
+                        Join Room
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
