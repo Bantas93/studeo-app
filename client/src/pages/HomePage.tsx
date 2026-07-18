ab46c878856baf8260d496e2d0c6a3302c884f35
Bantas93 <bantaz.mtdn@gmail.com>
2026-07-19 02:12:03 +0700

    modified homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router";
+
 export default function HompePage() {
-  return <div>Homepage</div>;
+  return (
+    <div className="min-h-screen p-6">
+      <div className="flex flex-col gap-4">
+        <div>
+          <Link to={"/room/create"} className="btn">
+            Create Room
+          </Link>
+        </div>
+
+        <div className="card w-96 bg-base-100 card-md shadow-sm">
+          <div className="card-body">
+            <h2 className="card-title">Topic : Cari rumus segitiga</h2>
+            <p>Mata pelajaran : Matematika</p>
+            <div className="justify-end card-actions">
+              <button className="btn btn-primary">Join Room</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
